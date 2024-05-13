900918f8ca33c2aae049099f48d8121894ee8aaf
Agustin Reseco Alonso <149005541+agustinresecodev@users.noreply.github.com>
2024-05-14 00:35:50 +0200

    edited package.json app.ts cors seeders UserRole data-source Role User api.routes server.ts index.d.ts types.ts

---
+++ src/routes/api.routes.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+//import usersRoutes from "./users.routes";
+//import baseRoutes from "./base.routes";
+
+
+const router = express.Router();
+
+//////////      API ROUTES      //////////////////
+
+// base routes
+//router.use("/", baseRoutes);
+
+// users routes
+//router.use("/users", usersRoutes);
