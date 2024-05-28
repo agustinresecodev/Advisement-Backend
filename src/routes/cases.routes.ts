b522facc018730ca6e4867b42448e7219d7099fa
Agustin Reseco Alonso <149005541+agustinresecodev@users.noreply.github.com>
2024-05-28 18:07:32 +0200

    added authorizeMiddleware to protected routes

--- src/routes/cases.routes.ts
+++ src/routes/cases.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { caseController } from '../controllers/caseController';
 import { authMiddleware } from '../middlewares/authMiddleware';
+import { authorizeMiddleware } from '../middlewares/authorize';
 
 //CREATE CONST WITH ROUTER METHOD
 const router = express.Router();
@@ -10,8 +11,8 @@ router.get('/',authMiddleware, caseController.getAllCases);
 router.get('/technician/',authMiddleware, caseController.getCasesByUser);
 router.get('/:id',authMiddleware, caseController.getCaseById);
 router.put('/:id',authMiddleware, caseController.editCase);
-router.delete('/:id',authMiddleware, caseController.deleteCase);
-router.post('/',authMiddleware, caseController.createCase);
+router.delete('/:id',authMiddleware, authorizeMiddleware(['administration']), caseController.deleteCase);
+router.post('/',authMiddleware,authorizeMiddleware(['administration']), caseController.createCase);
 
 
 
